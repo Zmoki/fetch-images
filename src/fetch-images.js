ce0e876dac4edcbbe72b546bf0f552e5a704aabc
Zarema Khalilova <zarema.khalilova@gmail.com>
2016-02-28 23:27:46 +0300

    Update tests and fetch-images.js

--- src/fetch-images.js
+++ src/fetch-images.js
@@ -1,18 +1,42 @@
 function fetchImages(images) {
   return new Promise((resolve, reject) => {
-    let img = new Image();
+    if(!images || !Array.isArray(images) || !images.length) {
+      reject('Images is not defined.');
+    }
 
-    img.addEventListener('load', () => {
-      resolve([
-        [img, 'completed']
-      ]);
+    const promises = [];
+
+    images.forEach(image => {
+      promises.push(new Promise((innerResolve, innerReject) => {
+        const isHTMLImageElement = image instanceof HTMLImageElement;
+
+        if(!isHTMLImageElement && (typeof image != 'string')) {
+          innerReject(`${image} is not image.`);
+        }
+
+        let img = isHTMLImageElement ? image : new Image();
+
+        img.addEventListener('load', () => {
+          innerResolve([img, 'completed']);
+        });
+
+        img.addEventListener('error', () => {
+          innerReject(`Error while load ${image}.`);
+        });
+
+        img.addEventListener('abort', () => {
+          innerReject(`Abort loading ${image}.`);
+        });
+
+        if(!isHTMLImageElement) {
+          img.src = image;
+        }
+      }));
     });
 
-    img.addEventListener('error', () => {
-      reject(img.src);
-    });
-
-    img.src = images[0];
+    Promise.all(promises)
+      .then(result => resolve(result))
+      .catch(error => reject(error));
   });
 }
 
--- test/fetch-images.spec.js
+++ test/fetch-images.spec.js
@@ -14,18 +14,78 @@ const getImageUrl = () => {
 describe('fetchImages', () => {
   it('return promise', () => {
     fetchImages().should.to.be.an.instanceOf(Promise);
-  })
+  });
 });
 
-
-describe('fetchImages resolve: ', () => {
-  describe('when args contain single real image url', () => {
-    it('return array', () => {
-      return fetchImages([getImageUrl()]).should.to.eventually.be.a('array');
+describe('fetchImages to be rejected', () => {
+  describe('with an Error', () => {
+    it('if args is not defined', () => {
+      return fetchImages().should.to.eventually.be.rejectedWith('Images is not defined.');
     });
-    it('return array with HTMLImageElement', () => {
+
+    it('if args is empty', () => {
+      return fetchImages([]).should.to.eventually.be.rejectedWith('Images is not defined.');
+    });
+
+    it('if args is not array', () => {
+      return fetchImages('blah').should.to.eventually.be.rejectedWith('Images is not defined.');
+    });
+  });
+  it('if images contain bad url', () => {
+    const images = [
+      getImageUrl(),
+      'blah',
+      getImageUrl()
+    ];
+    return fetchImages(images).should.to.eventually.be.rejectedWith('Error while load blah.');
+  });
+});
+
+describe('fetchImages to be resolved', () => {
+  describe('when args contain single real image url', () => {
+    it('with array', () => {
+      return fetchImages([getImageUrl()]).should.to.eventually.be.an('array');
+    });
+    it('with array with HTMLImageElement', () => {
       return fetchImages([getImageUrl()]).should.to.eventually.have.deep.property('[0][0]')
         .that.is.an.instanceOf(HTMLImageElement);
     });
+    it('with array with status', () => {
+      return fetchImages([getImageUrl()]).should.to.eventually.have.deep.property('[0][1]')
+        .that.is.equal('completed');
+    });
+  });
+
+  it('when args contain few links', () => {
+    const images = [
+      getImageUrl(),
+      getImageUrl()
+    ];
+    const result = fetchImages(images);
+
+    result.then(values => console.log(values));
+
+    return result.should.to.eventually.be.an('array');
+  });
+
+  it('when args contain few links and few HTMLImageElement', () => {
+    let img1 = new Image();
+    img1.src = getImageUrl();
+    let img2 = new Image();
+
+    const images = [
+      getImageUrl(),
+      img1,
+      getImageUrl(),
+      img2
+    ];
+
+    const result = fetchImages(images);
+
+    result.then(values => console.log(values));
+
+    img2.src = getImageUrl();
+
+    return result.should.to.eventually.be.an('array');
   });
 });
